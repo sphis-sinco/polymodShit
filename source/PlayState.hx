b9add9feec4bef5f72e4a64cc57c9e9b3b5d57bb
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-07 08:39:35 -0500

    I guess basic shit?

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,9 +1,18 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxState;
+import modding.ModInit;
 
 class PlayState extends FlxState
 {
+	override public function new()
+	{
+		super();
+
+		ModInit.initalizePolymod();
+	}
+
 	override public function create()
 	{
 		super.create();
@@ -12,5 +21,7 @@ class PlayState extends FlxState
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+		if (FlxG.keys.justReleased.BACKSPACE)
+			FlxG.switchState(ModMenu.new);
 	}
 }
---
+++ source/modding/ModList.hx
@@ -0,0 +1,7 @@
+package modding;
+
+class ModList
+{
+	public static var MOD_DIRECTORIES:Array<String> = [];
+	public static var ENABLED_MOD_DIRECTORIES:Array<String> = [];
+}
